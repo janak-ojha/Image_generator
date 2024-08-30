8a986fb4929c09c40e4c8488859c97a034d5066e
janak-ojha <janakrajojha66@gmail.com>
2024-08-31 01:00:36 +0530

    app

--- my-frontend/src/App.jsx
+++ my-frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import './App.css';
+import logo from "./assets/logo.svg"; // Correct import statement
 
-import './App.css'
 function App() {
   return (
-    <>
-     hello
-    </>
-  )
+    <Router>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
+        <Link to="/">
+          <img src={logo} alt="logo" className="h-10 w-10 object-contain" /> {/* Ensure this line is active */}
+          <p className="text-30">Replicate and Stability AI</p>
+        </Link>
+      </header>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
